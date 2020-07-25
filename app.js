108e24e2c420a07b6d81e5d6890976c7755422a7
safiullah777 <57296932+safiullah777@users.noreply.github.com>
2020-07-25 18:37:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+
+var text=document.getElementById('search');
+var b=document.getElementById('sec');
+var counter=0;
+
+
+function add(){
+
+if(!text.value==""){
+    counter++;
+    var c=document.createElement('div');
+    b.appendChild(c);
+    b.lastElementChild.setAttribute('id','record');
+    b.lastElementChild.innerHTML=text.value;
+    var rec=document.getElementById('record');
+    var del=document.createElement('button');
+    b.lastElementChild.appendChild(del);
+
+    b.lastElementChild.lastElementChild.innerHTML="delete"
+    var c=b.lastElementChild;
+    c.lastElementChild.setAttribute('id','dlt');
+    c.lastElementChild.setAttribute('class','btn btn-danger');
+    c.lastElementChild.setAttribute("onclick","deletes(this)");
+
+    document.getElementById('count').innerHTML="There are "+counter+" todos."
+    text.value="";
+
+}
+else{
+    alert("please enter todo.")
+
+}
+
+    
+}
+function deleteAll(){
+    counter=0;
+    b.innerHTML="";
+    document.getElementById('count').innerHTML=`There are ${counter} todos.`
+    
+
+
+}
+function deletes(e){
+    counter--;
+    document.getElementById('count').innerHTML="There are "+counter+" todos.";    
+}
